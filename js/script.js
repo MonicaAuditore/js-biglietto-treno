6a70bce86f64d1b6475f969e9b5956b220bc2b6a
MonicaAuditore <mauditore@live.it>
2022-12-19 14:51:15 +0100

    add_calcolo del tot. con due decimali

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@
 1. Chiedere all'utente il numero di chilometri da percorrere;
 2. chiedere all'utente l'età;
 3. calcolare il costo del biglietto in base ai km; Moltiplicando il costo di 1km (0.21 Euro) per il n° di km che deve percorrere;
-4. calcolare se l'utente è minorenne; ovvero se la sua età è <= a 17 e se è minorenne applicare lo sconto del 20% altrimenti no; Mostrare il costo;
-5. calcolare se l'utente è over 65: ovvero se la sua età è >= 65 e se è over 65 applicare lo sconto del 40% altrimenti no; Mostrare il costo;
-6. se l'utente non è ne minorenne ne maggiorenne il costo del biglietto resta invariato; Mostrare il costo;
+4. calcolare se l'utente è minorenne; ovvero se la sua età è <= a 17 e se è minorenne applicare lo sconto del 20% altrimenti no; Mostrare il costo finale con 2 decimali;
+5. calcolare se l'utente è over 65: ovvero se la sua età è >= 65 e se è over 65 applicare lo sconto del 40% altrimenti no; Mostrare il costo finale con 2 decimali;
+6. se l'utente non è ne minorenne ne maggiorenne il costo del biglietto resta invariato; Mostrare il costo finale con 2 decimali;
 0 ----- 18 ------ 65 ------
-7. mostrare il costo finale con 2 decimali per indicare i centesimi sul prezzo.*/
+*/
 
 let numeroKilometri;
 let etaUtente;
@@ -25,9 +25,9 @@ console.log(etaUtente);
 costoBiglietto = numeroKilometri * 0.21;
 console.log(costoBiglietto);
 
-// 4. calcolare se l'utente è minorenne; ovvero se la sua età è <= a 17 e se è minorenne applicare lo sconto del 20% altrimenti no; Mostrare il costo;
+// 4. calcolare se l'utente è minorenne; ovvero se la sua età è <= a 17 e se è minorenne applicare lo sconto del 20% altrimenti no; Mostrare il costo finale con 2 decimali;
 if (etaUtente <= 17) {
-  costoBiglietto = (costoBiglietto * 20) / 100;
+  costoBiglietto = ((costoBiglietto * 20) / 100).toFixed(2);
   costoBiglietto = prompt(
     "Sei minorenne, hai uno sconto del 20%, il costo del tuo biglietto è: ",
     costoBiglietto + " €"
@@ -35,9 +35,9 @@ if (etaUtente <= 17) {
   console.log(costoBiglietto);
 }
 
-// 5. calcolare se l'utente è over 65: ovvero se la sua età è >= 65 e se è over 65 applicare lo sconto del 40% altrimenti no; Mostrare il costo;
+// 5. calcolare se l'utente è over 65: ovvero se la sua età è >= 65 e se è over 65 applicare lo sconto del 40% altrimenti no; Mostrare il costo finale con 2 decimali;
 else if (etaUtente >= 65) {
-  costoBiglietto = (costoBiglietto * 40) / 100;
+  costoBiglietto = ((costoBiglietto * 40) / 100).toFixed(2);
   costoBiglietto = prompt(
     "Sei over 65, hai uno sconto del 40%, il costo del tuo biglietto è: ",
     costoBiglietto + " €"
@@ -45,13 +45,11 @@ else if (etaUtente >= 65) {
   console.log(costoBiglietto);
 }
 
-// 6. se l'utente non è ne minorenne ne maggiorenne il costo del biglietto resta invariato; Mostrare il costo;
+// 6. se l'utente non è ne minorenne ne maggiorenne il costo del biglietto resta invariato; Mostrare il costo finale con 2 decimali;
 else {
   costoBiglietto = prompt(
     "Il costo del tuo biglietto è: ",
-    costoBiglietto + " €"
+    costoBiglietto.toFixed(2) + " €"
   );
   console.log(costoBiglietto);
 }
-
-// 7. mostrare il costo finale con 2 decimali per indicare i centesimi sul prezzo.*/
